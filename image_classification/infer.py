85026468133f5f8a60e94625c80990359cf0c88c
Balazs Opra <balazs.o@t2.auto>
2024-01-02 13:45:38 +0900

    Visualize attention in inference script

--- image_classification/infer.py
+++ image_classification/infer.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 from typing import Optional
 
@@ -5,11 +6,14 @@ import cattrs
 import hydra
 import lightning as L
 import matplotlib.colors as mcolors
+import numpy as np
 import omegaconf
 import torch
 from attr import define
 from matplotlib import pyplot as plt
+from PIL import Image
 from torch.utils.data import DataLoader
+from torchvision import transforms
 
 from image_classification.data.dataset import CatsVsDogsDataset
 from image_classification.models.image_classifier_lightning import (
@@ -22,7 +26,7 @@ class Config:
     checkpoint_path: Path
     input_image_path: Path  # The filename must contain either "cat" or "dog", and this acts as the label.
     data_transform: dict  # The Hydra instantiation configuration for the torchvision transform to apply to the image.
-    attention_viz_outfile_path: Optional[Path]
+    attention_viz_outdir: Optional[Path]
 
 
 @hydra.main(config_path="config", config_name="infer_config")
@@ -47,41 +51,63 @@ def main(config_dict: dict | omegaconf.DictConfig):
     classification_score, attention = result
 
     print(f"Predicted class: {classification_score.item():.2f}")
+    if config.attention_viz_outdir is not None:
+        _plot_attention_per_head_save(
+            original_image=Image.open(config.input_image_path),
+            model_input_image=dataset[0][0],
+            attention_scores=attention,
+            output_filepath_prefix=config.attention_viz_outdir / f"{config.input_image_path.stem}_attention",
+        )
 
 
-def _plot_attention_per_head_save(tokens: list[str], attention_scores: torch.Tensor, output_filepath: Path):
+def _plot_attention_per_head_save(
+    original_image: Image, model_input_image: torch.Tensor, attention_scores: torch.Tensor, output_filepath_prefix: Path
+):
     """Plot the attention scores for each head, and save the plot.
 
     Args:
-        tokens: The tokens in the input text.
-        attention_scores: The attention scores, shape (num_heads, seq_len, seq_len).
-        filename_prefix: The prefix of the filename to save the plot to.
+        input_image: The input image, shape (1, 3, height, width).
+        attention_scores: The attention scores, shape (num_heads, num_patches, num_patches).
+        output_filepath: The prefix of the filename to save the plot to.
     """
-    num_heads = attention_scores.shape[0]
-    seq_len = len(tokens)
+    attention_scores = attention_scores.cpu().detach().squeeze(0)
+    num_patches = attention_scores.shape[1] - 1  # -1 for the class embedding
+    image_height = model_input_image.shape[1]
+    image_width = model_input_image.shape[2]
+    height_to_width_ratio = image_height / image_width
+    num_vertical_patches = math.sqrt(num_patches * height_to_width_ratio)
+    num_horizontal_patches = num_patches / num_vertical_patches
+    if num_vertical_patches % 1 != 0 or num_horizontal_patches % 1 != 0:
+        raise ValueError(
+            f"Number of patches ({num_patches}) must be divisible by the square root of the height to width ratio "
+            f"({height_to_width_ratio})"
+        )
+    num_vertical_patches = int(num_vertical_patches)
+    num_horizontal_patches = int(num_horizontal_patches)
+    patch_size = image_height / num_vertical_patches
+    assert patch_size == image_width / num_horizontal_patches
+    patch_size = int(patch_size)
 
-    # Creating a figure with subplots for each head
-    fig, axs = plt.subplots(num_heads, 1, figsize=(0.7 * seq_len, num_heads))
+    heatmap = np.zeros((image_width, image_width, 4))
+    for head_idx in range(attention_scores.shape[0]):
+        attention_image = attention_scores[head_idx, 0, 1:].view(num_vertical_patches, num_horizontal_patches)
+        for i in range(num_vertical_patches):
+            for j in range(num_horizontal_patches):
+                attention_score = attention_image[i, j].item()
+                heatmap[i * patch_size : (i + 1) * patch_size, j * patch_size : (j + 1) * patch_size, :] = [
+                    1,
+                    0,
+                    0,
+                    attention_score * 10.0,
+                ]
+        resized_original_image = transforms.Resize((image_width, image_height))(original_image)
+        plt.imshow(resized_original_image)
+        plt.imshow(heatmap, cmap="jet", alpha=1.0)
+        plt.axis("off")
 
-    for head in range(num_heads):
-        ax = axs[head] if num_heads > 1 else axs
-        ax.set_axis_off()
-        ax.set_title(f"Head {head+1}")
+        plt.savefig(f"{output_filepath_prefix}_head_{head_idx}png", bbox_inches="tight")
 
-        for i, token in enumerate(tokens):
-            color = mcolors.to_hex(plt.cm.Blues(attention_scores[head, 0, i]))
-            ax.text(i, 0, token, ha="center", va="center", color="black", backgroundcolor=color, fontsize=12)
-
-        # Setting the limits and aspect
-        ax.set_xlim(-0.5, seq_len - 0.5)
-        ax.set_ylim(-0.5, 0.5)
-        ax.set_aspect("equal", adjustable="box")
-
-    plt.tight_layout()
-
-    plt.savefig(output_filepath, bbox_inches="tight")
-
-    plt.show()
+        plt.show()
 
 
 if __name__ == "__main__":
